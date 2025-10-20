ea6f3c01c316ae02164ccf3597b1a326d4d53e75
lis0ownik <kkowalewski844@gmail.com>
2025-10-20 22:12:48 +0200

    work

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      }
+    }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  transformIgnorePatterns: [
+    'node_modules/(?!(your-esm-package)/)',
+  ],
+};
